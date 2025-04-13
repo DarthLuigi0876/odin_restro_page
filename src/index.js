30554bfce1a25503a3cc502486ff14fb78423d84
Ayush <invinciblefoxstar@gmail.com>
2025-04-13 19:03:45 +0530

    Added aboutpageloader and changed button names retirever

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { loadHomepage } from "./home";
 import { loadMenuPage } from "./menu";
+import { loadAboutPage } from "./about";
 
-const home = document.getElementById('home');
-const menu = document.getElementById('menu');
+const home = document.getElementById('homeButton');
+const menu = document.getElementById('menuButton');
+const about = document.getElementById('aboutButton');
 
 
 content = document.getElementById('content');
@@ -25,3 +27,8 @@ home.addEventListener('click', () => {
 menu.addEventListener('click', () => {
     loadPage(loadMenuPage());
 });
+
+about.addEventListener('click', () => {
+    loadPage(loadAboutPage());
+}
+);
