6bb70f430f7795cbb68442460fc8e438d2069ace
Ayush <invinciblefoxstar@gmail.com>
2025-04-13 13:39:52 +0530

    Added homepage loader

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-console.log("Hello, world!");
-alert("Hello, world!");
+import { loadHomepage } from "./home";
+
+content = document.getElementById('content');
+content.innerHTML = '';
+
+function loadPage(elements =[]) {
+    elements.forEach(element =>{
+        content.appendChild(element);
+    })
+}
+loadPage(loadHomepage());
