749cf08c5496046be33e33d60b5889e0d3ec12bb
Ayush <invinciblefoxstar@gmail.com>
2025-04-13 20:13:05 +0530

    Added logobutton

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import { loadHomepage } from "./home";
 import { loadMenuPage } from "./menu";
 import { loadAboutPage } from "./about";
+import './style.css';
 
 const home = document.getElementById('homeButton');
 const menu = document.getElementById('menuButton');
 const about = document.getElementById('aboutButton');
-
+const logo = document.getElementById('logoButton');
 
 content = document.getElementById('content');
 content.innerHTML = '';
@@ -20,6 +21,9 @@ const initialElements = loadHomepage();
 loadPage(initialElements);
 
 
+logo.addEventListener('click', () => {
+    loadPage(loadHomepage());
+})
 home.addEventListener('click', () => {
     loadPage(loadHomepage());
 });
